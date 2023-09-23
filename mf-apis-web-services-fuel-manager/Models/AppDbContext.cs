a2b7a1e1fc989b14ad3c40569266fb1deea5b041
Patrick Douglas <pdouglas.s@hotmail.com>
2023-09-23 13:33:50 -0300

    Criação migrations e banco de dados

--- mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -4,6 +4,12 @@ namespace mf_apis_web_services_fuel_manager.Models
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {                
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
 
     }
 }

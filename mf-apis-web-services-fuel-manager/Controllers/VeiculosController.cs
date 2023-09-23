970eb33d232180c153a47b74fa0add84d6a0a2a6
Patrick Douglas <pdouglas.s@hotmail.com>
2023-09-23 15:41:32 -0300

    Crud veiculos

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 namespace mf_apis_web_services_fuel_manager.Controllers
 {
@@ -13,15 +14,72 @@ namespace mf_apis_web_services_fuel_manager.Controllers
 
         public VeiculosController(AppDbContext context)
         {
-            _context = context; ;
+            _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
             var model = await _context.Veiculos.ToListAsync();
-
             return Ok(model);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if(model.AnoFabricacao <= 0 )
+            {
+                return BadRequest(new {message = "Ano de fabricação é obrigatório e deve ser informado"});
+            }
+            if (model.AnoModelo <= 0)
+            {
+                return BadRequest(new { message = "Ano modelo é obrigatório e deve ser informado" });
+            }
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) NotFound();
+
+            return Ok(model);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Veiculo model)
+        {
+            if (id != model.Id) return BadRequest();
+            var modeloDB = await _context.Veiculos.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modeloDB == null) return NotFound();
+
+            _context.Veiculos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Veiculos.FindAsync(id);
+
+            if (model == null) NotFound();
+
+            _context.Veiculos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }

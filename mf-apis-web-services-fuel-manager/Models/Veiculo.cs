ad9ecb7a7f71e7f4dc13f340896d9f29f6827adf
Patrick Douglas <pdouglas.s@hotmail.com>
2023-09-23 11:10:00 -0300

    Criacao modelo veiculo

---
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+    }
+}
